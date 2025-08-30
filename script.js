c76fffdbdfb4677b1986e27d0889d5f1fcf1e125
ggmikica <ggmikica@gmail.com>
2025-08-30 13:33:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -221,7 +221,7 @@
     // ----------- ODABIR ODGOVORA -----------
     function selectAnswer(choice, btn) {
       Array.from(buttonsEl.children).forEach(b => b.disabled = true);
-
+if (count >= 1) {  document.getElementById("pozdrav").style.display = "none";}
       if (choice === correctAnswer) {
         points += 5;
 
@@ -356,6 +356,7 @@ function posalji() {
 
 
 }
+
 
 
 
